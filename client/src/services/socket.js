bcb06eab4d5edbada6a0c7c7189ee52a2aa1e6b5
Hisham Khuram <hishamkhuram8@gmail.com>
2025-12-06 23:06:57 +0530

    users_error_fix

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -4,4 +4,8 @@ const URL = 'http://localhost:3000';
 
 export const socket = io(URL, {
     autoConnect: true,
+    reconnection: true,
+    reconnectionDelay: 1000,
+    reconnectionDelayMax: 5000,
+    reconnectionAttempts: 5
 });
